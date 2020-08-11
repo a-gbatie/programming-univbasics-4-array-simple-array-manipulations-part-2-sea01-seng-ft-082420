41b7812a98b6ed1b177fda3a5bcc2f00abaa3f8f
Ange Batie <ange.batie@gmail.com>
2020-08-11 05:22:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,7 @@ def using_delete(array, String)
   array.delete(String)
 end
 
-def using_delete_at(array, Integer)
-  array.delete_at(Integer)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
 
