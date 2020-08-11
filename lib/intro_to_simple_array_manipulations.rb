ced1d78417a39248259faad07feeb59d2653ef30
Ange Batie <ange.batie@gmail.com>
2020-08-11 05:21:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,16 @@ end
 def using_uniq(array)
   array.uniq 
 end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, String)
+  array.delete(String)
+end
+
+def using_delete_at(array, Integer)
+  array.delete_at(Integer)
+end
+
