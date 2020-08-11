599b150189433de96da4955fc3b596e7ee7bdbce
Ange Batie <ange.batie@gmail.com>
2020-08-11 05:22:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete(array, String)
-  array.delete(String)
+def using_delete(array, string)
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
